62831c8e8b54b32bbb9b12cdf2ccb893b7862895
khaled010 <hamrani.khaled1997@gmail.com>
2023-04-25 00:05:34 +0200

    init

---
+++ src/main/java/fr/tlsspring/TlsSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.tlsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlsSpringApplication.class, args);
+	}
+
+}
